d160604de1021d8d1ff471e1349badfae7b80e37
Jeff Oh <joh@elasticpath.com>
2014-02-09 16:07:12 -0800

    Removed unused Draw() method from Arrow

--- src/com/droidquest/decorations/Arrow.java
+++ src/com/droidquest/decorations/Arrow.java
@@ -1,7 +1,6 @@
 package com.droidquest.decorations;
 
 import java.awt.Color;
-import java.awt.Graphics;
 import java.io.Serializable;
 
 public class Arrow implements Serializable 
@@ -24,34 +23,6 @@ public Arrow(int X, int Y, int dir, int len, Color c)
 	direction = dir;
 	length = len;
 	color = c;
-  }
-
-public void Draw(Graphics g) 
-  {
-	g.setColor(color);
-	switch(direction)
-	  {
-	   case 0:
-	     g.drawLine(x,y,x-8,y+8);
-	     g.drawLine(x,y,x+8,y+8);
-	     g.drawLine(x,y,x,y+length);
-	     break;
-	   case 1:
-	     g.drawLine(x,y,x-8,y-8);
-	     g.drawLine(x,y,x-8,y+8);
-	     g.drawLine(x,y,x-length,y);
-	     break;
-	   case 2:
-	     g.drawLine(x,y,x-8,y-8);
-	     g.drawLine(x,y,x+8,y-8);
-	     g.drawLine(x,y,x,y-length);
-	     break;
-	   case 3:
-	     g.drawLine(x,y,x+8,y-8);
-	     g.drawLine(x,y,x+8,y+8);
-	     g.drawLine(x,y,x+length,y);
-	     break;
-	  }
   }
 
     public Color getColor() {
